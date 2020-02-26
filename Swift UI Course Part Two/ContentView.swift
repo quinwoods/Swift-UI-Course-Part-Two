9b22306bd3e79e7923b45fc92de733d624fa67e0
Quin'darius <quinwoodz@gmail.com>
2020-02-25 20:28:31 -0500

    Initial Commit

---
+++ Swift UI Course Part Two/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Course Part Two
+//
+//  Created by Quin’darius Lyles-Woods on 2/25/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
